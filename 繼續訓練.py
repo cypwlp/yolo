afff5b979bbd0453c113d45a44488b3cd35252fa
UltralyticsAssistant <web@ultralytics.com>
2026-03-05 04:12:53 +0000

    Auto-format by https://ultralytics.com/actions

--- 繼續訓練.py
+++ 繼續訓練.py
@@ -2,7 +2,7 @@
 model = YOLO(r"C:\Users\20250923\Desktop\ultralytics-main\runs\detect_optimized_1107_1657\weights\last.pt")
 model.train(
     resume=True,  # 继续训练
-    epochs=150,   # 总epochs
+    epochs=150,  # 总epochs
     patience=50,  # 增加耐心值
-    plots=True    # 生成完整图表
+    plots=True,  # 生成完整图表
 )
